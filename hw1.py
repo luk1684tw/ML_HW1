204b43d328c5f43ff8b46b2b54c9e09546b95e88
luk1684tw <luk1684tw@gmail.com>
2019-09-25 15:57:22 +0800

    parser done

--- hw1.py
+++ hw1.py
@@ -1,15 +1,30 @@
+import argparse
+
 sample_in = './sample_in.txt'
 
 def read_testcase(sample_in):
     test_cases = open(sample_in, 'r')
     data_points = list()
     for data_point in test_cases:
-        data_array = tuple(data_point.split(','))
+        data_array = tuple(data_point.strip().split(','))
         data_points.append(data_array)
     return data_points
 
-def lu_decompose()
+def lu_decompose():
+    pass
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--path", type=str, help="Path of testcase")
+    parser.add_argument("-n", "--base", type=int, help="polynomial bases of predicted regression")
+    parser.add_argument("-l", "--lambda", type=int, help="lambda of LSE")
+    args = parser.parse_args()
+
+    return args
 
 if __name__ == "__main__":
-    data_points = read_testcase(sample_in)
+    args = get_args()
+    data_points = read_testcase(args.path)
+    print (data_points)
+
     
