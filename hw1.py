25c752ffdee973cd7c08940572e55661ee007414
luk1684tw <luk1684tw@gmail.com>
2019-09-26 19:40:14 +0800

    matrix operation implementing

--- hw1.py
+++ hw1.py
@@ -1,5 +1,25 @@
 import argparse
-import numpy as np
+
+class Matrix():
+    def __init__(self, data):
+        self.data = data
+        self.rows = len(data)
+        self.cols = len(data[0])
+        return
+    
+    def mul(self, data):
+        operand = data.reverse().data
+        for 
+    def reverse(self):
+        res = [[0 for j in range(self.rows)] for i in range(self.cols)]
+        for i in range(self.cols):
+            for j in range(self.rows):
+                res[i][j] = self.data[j][i]
+
+        return Matrix(res)
+
+    def inverser(self):
+        pass
 
 def read_testcase(sample_in):
     test_cases = open(sample_in, 'r')
@@ -7,21 +27,25 @@ def read_testcase(sample_in):
     for data_point in test_cases:
         data_array = tuple(data_point.strip().split(','))
         data_points.append(data_array)
+    
     return data_points
 
 def lu_decompose():
     pass
 
-def lse(data_points, lamb):
+def lse(data_points, base, lamb):
     # print (f'Processing LSE with data points: {data_points} and lambda {lamb}')
 
     A = list()
     for x, y in data_points:
         row = list()
-        for i in range(lamb-1, -1, -1):
+        for i in range(base-1, -1, -1):
             row.append(float(x)**i)
         A.append(row)
-    print (A)
+
+    A = Matrix(A)
+    return
+
 
 def get_args():
     parser = argparse.ArgumentParser()
@@ -36,6 +60,7 @@ if __name__ == "__main__":
     args = get_args()
     data_points = read_testcase(args.path)
 
-    lse(data_points, args.lamb)
-
+    lse(data_points, args.base, args.lamb)
+    test = Matrix([[1, 3, 5], [2, 4, 6]])
+    print (test.reverse().data)
     
