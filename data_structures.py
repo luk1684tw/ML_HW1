b21fe7117a88b69713db0704c34474f891ead8d1
luk1684tw <luk1684tw@gmail.com>
2019-09-27 23:19:59 +0800

    part1 completeed

---
+++ data_structures.py
@@ -0,0 +1,71 @@
+
+class Matrix():
+    def __init__(self, data):
+        self.data = data
+        self.rows = len(data)
+        self.cols = len(data[0])
+        return
+
+    def add(self, data):
+        ans = list()
+        for i in range(self.rows):
+            ans.append([a + b for a, b in zip(self.data[i], data.data[i])])
+
+        return Matrix(ans)
+
+    def mul(self, data):
+        operand = data.reverse().data
+        ans = list()
+        for i in range(self.rows):
+            cur_row = list()
+            for j in range(data.cols):
+                cur_row.append(sum([a*b for a, b in zip(self.data[i], operand[j])]))
+            ans.append(cur_row)
+
+        return Matrix(ans)
+
+    def reverse(self):
+        res = [[0 for j in range(self.rows)] for i in range(self.cols)]
+        for i in range(self.cols):
+            for j in range(self.rows):
+                res[i][j] = self.data[j][i]
+
+        return Matrix(res)
+
+    def lu_decompose(self, b):
+        L, U = Matrix.make_identity(self.rows, 1).data[:], self.data[:]
+        for i in range(self.rows-1):
+            for j in range(i+1, self.rows):
+                factor = U[j][i] / U[i][i]
+                L[j][i] = factor
+                U[j] = [j - factor*i for i, j in zip(U[i], U[j])]
+
+        y = [0 for i in range(len(L[0]))]
+        for i in range(len(L)):
+            front_sum = 0
+            for j in range(i):
+                front_sum += L[i][j]*y[j]
+            y[i] = (b[i] - front_sum) / L[i][i]
+
+        x = [0 for i in range(len(y))]
+        for i in range(len(y)-1, -1, -1):
+            back_sum = 0
+            for j in range(len(y)-1, i, -1):
+                back_sum += U[i][j] * x[j]
+            
+            x[i] = (y[i] - back_sum) / U[i][i]
+
+        return x
+
+    @staticmethod
+    def inverse(matrix):
+        identity = Matrix.make_identity(matrix.rows, 1)
+        ans = list()
+        for i in range(matrix.rows):
+            ans.append(matrix.lu_decompose(identity.data[i]))
+        
+        return Matrix(ans).reverse()
+        
+    @staticmethod
+    def make_identity(n, lamb):
+        return Matrix([[lamb if i == j else 0 for j in range(n)] for i in range(n)])
--- hw1.py
+++ hw1.py
@@ -1,52 +1,6 @@
 import argparse
 
-class Matrix():
-    def __init__(self, data):
-        self.data = data
-        self.rows = len(data)
-        self.cols = len(data[0])
-        return
-
-    def add(self, data):
-        ans = list()
-        for i in range(self.rows):
-            ans.append([a + b for a, b in zip(self.data[i], data.data[i])])
-
-        return Matrix(ans)
-
-    def mul(self, data):
-        operand = data.reverse().data
-        ans = list()
-        for i in range(self.rows):
-            cur_row = list()
-            for j in range(data.cols):
-                cur_row.append(sum([a*b for a, b in zip(self.data[i], operand[j])]))
-            ans.append(cur_row)
-
-        return Matrix(ans)
-
-    def reverse(self):
-        res = [[0 for j in range(self.rows)] for i in range(self.cols)]
-        for i in range(self.cols):
-            for j in range(self.rows):
-                res[i][j] = self.data[j][i]
-
-        return Matrix(res)
-
-    def inverse(self):
-        L, U = make_identity(self.rows, 1), self.data[:]
-        for i in range(self.rows-1):
-            for j in range(i+1, self.rows):
-                factor = U[i][j] / U[i][i]
-                L.data[j][i] = factor
-                U[j] = [j - factor*i for i, j in zip(U[i], U[j])]
-                print (U[j])
-        # print (L.data, U)
-
-        return
-                
-def make_identity(n, lamb):
-    return Matrix([[lamb if i == j else 0 for j in range(n)] for i in range(n)])
+from data_structures import Matrix
 
 
 def read_testcase(sample_in):
@@ -61,17 +15,23 @@ def read_testcase(sample_in):
 def lse(data_points, base, lamb):
     # print (f'Processing LSE with data points: {data_points} and lambda {lamb}')
     A = list()
+    b = list()
     for x, y in data_points:
         row = list()
         for i in range(base-1, -1, -1):
             row.append(float(x)**i)
         A.append(row)
+        b.append([float(y)])
 
+    b = Matrix(b)
     A = Matrix(A)
-    identity = make_identity(A.cols, lamb)
+    identity = Matrix.make_identity(A.cols, lamb)
+    A_reverse = A.reverse()
+    
     A = A.reverse().mul(A).add(identity)
+    A = Matrix.inverse(A)
 
-    return
+    return A.mul(A_reverse).mul(b).data
 
 def get_args():
     parser = argparse.ArgumentParser()
@@ -86,6 +46,5 @@ if __name__ == "__main__":
     args = get_args()
     data_points = read_testcase(args.path)
 
-    lse(data_points, args.base, args.lamb)
-    op1 = Matrix([[-1, 2, -1], [1, -4, 6] ,[-2, 6, -6]])
-    op1.inverse()
+    ans = lse(data_points, args.base, args.lamb)
+    print (ans)
